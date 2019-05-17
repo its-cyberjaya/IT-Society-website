fdb843d7e918d76a7c8222d19d6536fee4c5f429
Evangelista Grace <evangrace98@gmail.com>
2019-05-17 19:46:40 +0800

    added events page and made css edits

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20190517132201';
+const version = '20190517193527';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/random/2019/05/16/Lorem-ipsum/","/general/2019/03/10/lorem-ipsum-2/","/general/2018/08/22/hello-world/","/about/","/categories/","/elements/","/blog/","/","/manifest.json","/assets/search.json","/search/","/assets/styles.css","/redirects.json","/feed.xml","/sitemap.xml","/robots.txt","", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/random/2019/05/16/Lorem-ipsum/","/general/2019/03/10/lorem-ipsum-2/","/general/2018/08/22/hello-world/","/about/","/categories/","/elements/","/events/","/blog/","/","/manifest.json","/assets/search.json","/search/","/assets/styles.css","/redirects.json","/feed.xml","/sitemap.xml","/robots.txt","", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
